63e9f97edec0bd8b0ab411d190d1c27e4296d028
hyeon <hyeon74>
2022-04-14 16:05:59 +0900

    test

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -11,9 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	private final static String TAG = "HttpControllerTest:";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().password("1234").email("test@gmail.com").build();
+
+		System.out.println(TAG + "lombok:" + m.getId());
+		System.out.println(TAG + "email:" + m.getEmail());
+		
+		m.setId(5000);
+		m.setEmail("hyeon@gmail.com");
+		
+		System.out.println(TAG + "lombok:" + m.getId());
+		System.out.println(TAG + "email:" + m.getEmail());
+		
+		
+		return "lombok Test 완료";
+	}
+	
 	@GetMapping("/http/get")
-	public String getTest(@RequestParam int id) {
-		return "get 요청:" + id;
+	public String getTest(Member m) {
+		System.out.println(TAG + "getter:" + m.getId());
+		System.out.println(TAG + "getter:" + m.getPassword());
+		System.out.println(TAG + "getter:" + m.getEmail());
+		
+		return "get 요청:" + m.getId() + ":" + m.getPassword()  + ":" + m.getEmail() ;
 	}
 	
 	@PostMapping("/http/post")
