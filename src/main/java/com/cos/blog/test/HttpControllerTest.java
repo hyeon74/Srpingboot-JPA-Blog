23fe5d7893176459393ccd07f6377e196e5926fc
hyeon74 <hyeon74@gmail.com>
2022-04-18 12:05:22 +0900

    Update HttpControllerTest.java

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -1,18 +1,47 @@
 package com.cos.blog.test;
 
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.blog.repository.BoardRepository;
+import com.cos.blog.repository.UserRepository;
 
 @RestController
 public class HttpControllerTest {
 	
 	private final static String TAG = "HttpControllerTest:";
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	BoardRepository boardRepository;
+	
+	@GetMapping("/getAllUser")
+	public String userTest() {
+		
+		return userRepository.selectAllUser().toString();
+	}
+	
+	@GetMapping("/getUser")
+	public String userTest2() {
+		
+		return userRepository.selectUser(1).toString();
+	}
+	
+	
+	
+	@GetMapping("/baordTest")
+	public String baartTest() {
+
+		return boardRepository.selectAllBoard().toString();
+	}
+	
 	
 	@GetMapping("/http/lombok")
 	public String lombokTest() {
@@ -26,6 +55,7 @@ public class HttpControllerTest {
 		
 		System.out.println(TAG + "lombok:" + m.getId());
 		System.out.println(TAG + "email:" + m.getEmail());
+		
 		
 		return "lombok Test 완료";
 	}
